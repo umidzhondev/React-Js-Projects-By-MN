f3ebf1d22bb0f185ef8e3a62ff3e0f8e967c6da3
umidzhondev <umidzhonfullstackdev@gmail.com>
2022-07-27 14:47:19 +0600

    Lesson-25: Pagination

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from 'react';
+import Main from './components/pagination/Main';
 import "./App.css";
-import Navbar from "./components/cocktails/Navbar";
-import Home from "./components/cocktails/pages/Home";
-import About from "./components/cocktails/pages/About";
-import Error from "./components/cocktails/pages/Error";
-import SingleCocktail from "./components/cocktails/pages/SingleCocktail";
-
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path='cocktail/:id' element={<SingleCocktail/>}/>
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </Router>
+     <div>
+       <Main/>
+     </div>
   );
 }
 
